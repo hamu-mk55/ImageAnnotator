f53a095627c51a6bd4e5e1acba12e80e354cccce
hamu-mk55 <153355786+hamu-mk55@users.noreply.github.com>
2025-08-03 20:03:37 +0900

    Add files via upload

--- src/db.py
+++ src/db.py
@@ -15,46 +15,68 @@ class AnnotationDB:
 
     def create_table(self):
         self.conn.execute('''CREATE TABLE IF NOT EXISTS annotations (
-            image_path TEXT,
+            filename TEXT,
             x REAL,
             y REAL,
             width REAL,
             height REAL,
-            rect_label TEXT
+            rect_label TEXT,
+            img_label TEXT
         )''')
         self.conn.commit()
 
-    def save_annotation(self, image_path, rect, rect_label):
+    def save_annotation(self, img_path, rect, rect_label):
+        label, filename = self._get_label_and_filename(img_path)
+
         self.conn.execute(
-            "INSERT INTO annotations (image_path, x, y, width, height, rect_label) VALUES (?, ?, ?, ?, ?, ?)",
-            (image_path, rect.x(), rect.y(), rect.width(), rect.height(), rect_label))
+            "INSERT INTO annotations (filename, x, y, width, height, rect_label, img_label) VALUES (?, ?, ?, ?, ?, ?,?)",
+            (filename, rect.x(), rect.y(), rect.width(), rect.height(), rect_label, label))
         self.conn.commit()
 
-    def load_annotations(self, image_path):
-        cursor = self.conn.execute("SELECT x, y, width, height, rect_label FROM annotations WHERE image_path=?",
-                                   (image_path,))
-        return [(QRectF(x, y, w, h), label) for x, y, w, h, label in cursor.fetchall()]
+    def load_annotations(self, img_path):
+        label, filename = self._get_label_and_filename(img_path)
 
-    def delete_annotation(self, image_path, rect, tol=1.0):
+        cursor = self.conn.execute("SELECT x, y, width, height, rect_label FROM annotations WHERE filename=?",
+                                   (filename,))
+        return [(QRectF(x, y, w, h), rect_label) for x, y, w, h, rect_label in cursor.fetchall()]
+
+    def delete_annotation(self, img_path, rect, tol=1.0):
+        label, filename = self._get_label_and_filename(img_path)
+
         self.conn.execute(
-            '''DELETE FROM annotations WHERE image_path=? AND
+            '''DELETE FROM annotations WHERE filename=? AND
                ABS(x - ?) < ? AND ABS(y - ?) < ? AND ABS(width - ?) < ? AND ABS(height - ?) < ?''',
-            (image_path, rect.x(), tol, rect.y(), tol, rect.width(), tol, rect.height(), tol)
+            (filename, rect.x(), tol, rect.y(), tol, rect.width(), tol, rect.height(), tol)
         )
         self.conn.commit()
 
-    def delete_all_annotations(self, image_path):
-        self.conn.execute("DELETE FROM annotations WHERE image_path=?", (image_path,))
+    def update_label(self, img_path):
+        label, filename = self._get_label_and_filename(img_path)
+
+        self.conn.execute(
+            "UPDATE annotations SET img_label=? WHERE filename=?",
+            (label, filename)
+        )
+        self.conn.commit()
+
+    def delete_all_annotations(self, img_path):
+        label, filename = self._get_label_and_filename(img_path)
+
+        self.conn.execute("DELETE FROM annotations WHERE filename=?", (filename,))
         self.conn.commit()
 
     def export_to_csv(self, csv_path):
-        cursor = self.conn.execute("SELECT image_path, x, y, width, height, rect_label FROM annotations")
+        cursor = self.conn.execute("SELECT filename, x, y, width, height, rect_label, img_label FROM annotations")
         with open(csv_path, "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
-            writer.writerow(["image_path", "label", "x", "y", "width", "height", "rect_label"])
+            writer.writerow(["filename", "img_label", "x", "y", "width", "height", "rect_label"])
             for row in cursor:
-                image_path, x, y, width, height, rect_label = row
-                base_name = os.path.basename(image_path)
-                label = os.path.basename(os.path.dirname(image_path))
+                filename, x, y, width, height, rect_label, img_label = row
 
-                writer.writerow([base_name, label, x, y, width, height, rect_label])
+                writer.writerow([filename, img_label, x, y, width, height, rect_label])
+
+    def _get_label_and_filename(self, img_path):
+        filename = os.path.basename(img_path)
+        label = os.path.basename(os.path.dirname(img_path))
+
+        return label, filename
